1cfa6ef5b1276370e42f5a094425e579b09483d9
pablo.felicissimo <pablofelicissimo@opah.com.br>
2017-08-21 12:08:11 -0300

    Correção do cors

--- api.php
+++ api.php
@@ -1,8 +1,29 @@
 <?php 
+	
+	// Allow from any origin
+	if (isset($_SERVER['HTTP_ORIGIN'])) {
+		header("Access-Control-Allow-Origin: *");
+		header('Access-Control-Allow-Credentials: true');
+		header('Access-Control-Max-Age: 86400');    // cache for 1 day
+	}
+
+	// Access-Control headers are received during OPTIONS requests
+	if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+
+		if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
+			header("Access-Control-Allow-Methods: GET, POST, OPTIONS");         
+
+		if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+			header("Access-Control-Allow-Headers:        {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+
+		exit(0);
+	}
+
+
 	$token = "227d3e9787e8bbccc51542d944a600fa691556420cc304863375feee6a15de0c";
 
 	header('Content-Type: application/json');
-	
+
 	if ($_SERVER['REQUEST_METHOD'] === 'POST') {
      	$input = json_decode(file_get_contents('php://input'), TRUE); 
      	$continuar = true;
